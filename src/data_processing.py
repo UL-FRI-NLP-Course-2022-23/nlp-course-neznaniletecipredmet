275ed6d2a2d7888535d619e26e52176de6ba7c16
Gal petkovšek <gp1914@student.uni-lj.si>
2023-05-21 17:29:13 +0200

    dataset preparation scripts

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,6 +1,11 @@
 import json
 import csv
 import os
+import nltk
+nltk.download('punkt')
+nltk.download('slovene')
+
+from nltk.tokenize import sent_tokenize
 
 def remove_new_lines(txt: str) -> str:
     # Replace new lines
@@ -13,7 +18,7 @@ def remove_new_lines(txt: str) -> str:
 
 def make_dataset_surrounding_words(num_words_before: int, num_words_after: str, stories_dir: str,
                                    positions_dir: str, sentiment_dir: str, result_path: str
-                                   ) -> None:
+                                   separate: bool) -> None:
     dataset = []
 
     for filename in os.listdir(stories_dir):
@@ -72,7 +77,74 @@ def make_dataset_surrounding_words(num_words_before: int, num_words_after: str, 
                 cumulative_string += " "
                 cumulative_string += string_after
             
-            dataset.append([cumulative_string, sentiment])
+                if(separate):
+                    dataset.append([cumulative_string, sentiment])
+                    cumulative_string = ""
+
+            if(not separate):
+                dataset.append([cumulative_string, sentiment])
+
+    with open(result_path, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerows(dataset)
+
+def make_dataset_sentences(stories_dir: str, positions_dir: str, sentiment_dir: str,
+                           result_path: str, separate: bool) -> None:
+    dataset = []
+
+    for filename in os.listdir(stories_dir):
+        print(filename, flush=True)
+
+        f = os.path.join(stories_dir, filename)
+        with open(f, encoding='utf-8') as f_story:
+            text = f_story.read()
+            text = remove_new_lines(text)
+
+        filename = filename.replace(".txt", ".json")
+        f = os.path.join(positions_dir, filename)
+        with open(f, encoding='utf-8') as json_file:
+            all_positions = json.load(json_file)
+
+        f = os.path.join(sentiment_dir, filename)
+        with open(f, encoding='utf-8') as json_file:
+            sentiments = json.load(json_file)
+
+        for character in all_positions:
+            positions = all_positions[character]
+            sentiment = sentiments[character]
+
+
+            cumulative_string = ""
+
+            for position in positions:
+                while(position[0]-1 > 0 and text[position[0]-1] != " "):
+                    position[0] -= 1
+                while(position[1] < len(text) and text[position[1]] != " "):
+                    position[1] += 1
+                text_before = text[:position[0]]
+                text_after = text[position[1]:]
+
+                if(len(cumulative_string) != 0):
+                    cumulative_string += " $ "
+
+                if(0 < len(text_before)):
+                    sentences_before = sent_tokenize(text_before, language='slovene')
+                    cumulative_string += sentences_before[-1]
+                    cumulative_string += " "
+
+                chosen_word = text[position[0]:position[1]]
+                cumulative_string += chosen_word
+                
+                if(0 < len(text_after)):
+                    sentences_after = sent_tokenize(text_after, language='slovene')
+                    cumulative_string += sentences_after[0]
+
+                if(separate):
+                    dataset.append([cumulative_string, sentiment])
+                    cumulative_string = ""
+
+            if(not separate):
+                dataset.append([cumulative_string, sentiment])
 
     with open(result_path, 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
--- src/test.py
+++ src/test.py
@@ -1,7 +1,11 @@
-from data_processing import make_dataset_surrounding_words, remove_all_new_lines
+from data_processing import make_dataset_surrounding_words, remove_all_new_lines, make_dataset_sentences
 
 #remove_all_new_lines()
 
-make_dataset_surrounding_words(1, 1, "../data/farytales/stories_to_extract", "../data/farytales/location_gt",
+# make_dataset_surrounding_words(1, 1, "../data/farytales/stories_to_extract", "../data/farytales/location_gt",
+#                                "../data/farytales/character_sentiment",
+#                                "../data/farytales/sentiment_dataset/dataset.csv")
+
+make_dataset_sentences("../data/farytales/stories_to_extract", "../data/farytales/location_gt",
                                "../data/farytales/character_sentiment",
-                               "../data/farytales/sentiment_dataset/dataset.csv")
+                               "../data/farytales/sentiment_dataset/dataset_separate.csv", True)
